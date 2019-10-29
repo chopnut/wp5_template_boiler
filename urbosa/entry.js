26e0c06135b6dcae95b3d144c9b267b98586b9bc
Ernani Danting <danny@lancemontana.com.au>
2019-10-30 05:00:23 +1000

    Edited

--- urbosa/entry.js
+++ urbosa/entry.js
@@ -1,6 +1,3 @@
-require('./src/lib/semantic-ui/transition.min.js')
-require('./src/lib/semantic-ui/visibility.min.js')
-
-import './src/lib/semantic-ui/reset.min.css';
-import './src/lib/semantic-ui/grid.min.css';
-import './src/lib/semantic-ui/transition.min.css';
+require('./src/lib/semantic-ui/semantic.min.js')
+import('./src/lib/normalize.min.css')
+import('./src/lib/semantic-ui/semantic.min.css')
