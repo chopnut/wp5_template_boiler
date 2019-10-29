55cf134d1560ef2272608887161d09da0744465c
Ernani Danting <danny@lancemontana.com.au>
2019-10-30 05:01:53 +1000

    Edited

--- urbosa/header.php
+++ urbosa/header.php
@@ -6,7 +6,6 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="icon" href="<?= get_template_directory_uri() . '/img/favicon.png' ?>">
-  <title>WP Website</title>
   <?php wp_head(); ?>
 
 </head>
