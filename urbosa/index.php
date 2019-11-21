bc29a3eabe661284d52b836d20ee69a0a1242172
Ernani Danting <danny@lancemontana.com.au>
2019-11-21 12:21:01 +1000

    Edit

--- urbosa/index.php
+++ urbosa/index.php
@@ -1,17 +1,13 @@
 <?php get_header(); ?>
 <article>
   <?php
-  /**
-   * Load the feature you want to see based on its functionality
-   */
 
-  /**
-   * locate_template: Load a template similar to get_template_part()
-   * Use this in your plugin to load the theme template file first before 
-   * loading your default template file.Returns '' empty string if not found
-   */
-  require_once(locate_template(['funcs/page-templates/parts/custom-login.php']));
-
+    if(have_posts()){
+      while(have_posts()){
+        the_post();
+        the_content();
+      }
+    }
 
   ?>
 </article>
