780adc815b10af91d2369d2a4a2629a2c5abd763
Ernani Danting <danny@lancemontana.com.au>
2019-11-21 11:27:46 +1000

    Edited

--- urbosa/functions.php
+++ urbosa/functions.php
@@ -1,7 +1,7 @@
 <?php
-require_once("funcs/helpers.php");
-require_once("funcs/cssjs.php");
-require_once("funcs/template-feature.php");
+require_once("inc/helpers.php");
+require_once("inc/cssjs.php");
+require_once("inc/template-feature.php");
 
 // Boiler-plate sample of all mostly used feature
-require_once('funcs/func-misc.php');
+require_once('inc/func-misc.php');
