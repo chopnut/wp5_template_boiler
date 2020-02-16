57c376ba11bd8b192783a20ceabe334e6bb9a514
Ernani Danting <danny@lancemontana.com.au>
2020-02-16 20:42:04 +1000

    Edit

--- urbosa/inc/acf-blocks/cb_example.php
+++ urbosa/inc/acf-blocks/cb_example.php
@@ -1,5 +1,17 @@
 <?php 
+//--------------------------------------------------------------------------
+// Create class attribute allowing for custom "className" and "align" values.
+// This will check alignment as well
+$className = 'cb_example';
+if( !empty($block['className']) ) {
+    $className .= ' ' . $block['className'];
+}
+if( !empty($block['align']) ) {
+    $className .= ' align' . $block['align'];
+}
+//-------------------------------------------------------------------------
 
-echo 'Test';
+
+echo 'Test example block display.';
 
 ?>
