92fa9ebada4e052c80be895fc1137f4a71498596
Ernani Danting <danny@lancemontana.com.au>
2020-02-17 05:31:40 +1000

    edit

--- urbosa/functions.php
+++ urbosa/functions.php
@@ -2,7 +2,7 @@
 require_once("inc/helpers.php");
 require_once("inc/cssjs.php");
 require_once("inc/template-feature.php");
-require_once("inc/register-blocks.php");
+require_once("inc/register-acf-blocks.php");
 
 // Boiler-plate sample of all mostly used feature
 require_once('inc/func-misc.php');
---
+++ urbosa/inc/register-acf-blocks.php
@@ -0,0 +1,35 @@
+<?php 
+/* Register blocks ACF only */
+if(!function_exists('acf_register_block')){
+  //----------------------------------------------
+  function register_blocks(){
+    /* 
+      Instruction: Copy the acf_register_block , 1 per each block set the render template.
+      Create the template php file in the blocks folder. Icon should be dashicon of wordpress without the 'dashicons'
+    */
+    // 1. Custom block example
+    acf_register_block(array(
+      'name'=> 'cb_example',
+      'title'=> __('Custom Block Example'),
+      'description'=>__('My example block'),
+      'render_template'=> 'inc/acf-blocks/cb_example.php',
+      'category'=> 'urbosa-blocks',
+      'icon'	=> 'layout',
+      'keywords'=> array( 'Example' ),
+    ));
+  }
+
+  add_action('acf/init', 'register_blocks');
+  //----------------------------------------------
+  function theme_category_block($categories,$post){
+    return array_merge($categories, array(
+      array(
+        'slug'=>'urbosa-blocks',
+        'title'=>__('Theme','urbosa-blocks')
+      )
+      ));
+  }
+  add_filter('block_categories','theme_category_block',10,2);
+}
+
+?>
