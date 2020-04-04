7e4ff2416703dbd074d5677fcaa2c10a11c8c26b
Ernani Danting <danny@lancemontana.com.au>
2020-04-05 07:22:52 +1000

    Edit

--- urbosa/functions.php
+++ urbosa/functions.php
@@ -1,8 +1,6 @@
 <?php
 require_once("inc/template-helpers.php");
 require_once("inc/template-feature.php");
+require_once('inc/template-main.php');
 require_once("inc/register-acf-blocks.php");
 require_once("inc/cssjs.php");
-
-// Boiler-plate sample of all mostly used feature
-require_once('inc/template-misc.php');
