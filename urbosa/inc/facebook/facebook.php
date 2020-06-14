e37457cf34678478a74eae3dfaab6ef75dc04af9
Ernani Danting <danny@lancemontana.com.au>
2020-06-14 20:52:10 +1000

    Edited

--- urbosa/inc/facebook/facebook.php
+++ urbosa/inc/facebook/facebook.php
@@ -4,9 +4,12 @@
   */
   class Facebook {
     private $html;
+    private $host;
+    
+    /* Messenger options */
     private $messengerPageID;
     private $messengerThemeColor;
-    private $host;
+    private $messengerGreeting;
 
     function __construct($appID='',$version='v7.0'){
       ob_start();
@@ -42,6 +45,7 @@
       /* Add facebook messenger to the page */
       /* Prerequisite: 
          Get facebook page id, and whitelist domain
+         More options: https://developers.facebook.com/docs/messenger-platform/discovery/customer-chat-plugin
       */
       ?>
       <script>
@@ -56,14 +60,20 @@
       <div class="fb-customerchat"
         attribution=setup_tool
         page_id="<?=$this->messengerPageID?>"
+        logged_in_greeting="<?=$this->messengerGreeting?>"
         theme_color="<?=$this->messengerThemeColor?>">
       </div>
       <?php
     }
     public function login(){}
-    public function addMessenger($pageID='',$themeColor='#0084ff'){
-      $this->messengerPageID = $pageID;
-      $this->messengerThemeColor = $themeColor;
+    public function addMessenger(
+        $pageID='',
+        $greeting="Hi!How can we help you?",
+        $themeColor='#0084ff' 
+    ){
+      $this->messengerPageID      = $pageID;
+      $this->messengerThemeColor  = $themeColor;
+      $this->messengerGreeting    = $greeting;
       add_action('wp_body_open', array($this,'messenger'));
     }
     public function addLogin(){
