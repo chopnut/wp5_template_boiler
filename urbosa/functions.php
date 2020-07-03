1b20c49b6cc289a882d876acc0e1476805dbf4c8
Ernani Danting <danny@lancemontana.com.au>
2020-07-03 10:47:50 +1000

    Edited

--- urbosa/functions.php
+++ urbosa/functions.php
@@ -1,7 +1,7 @@
 <?php
-require_once("inc/template-feature.php");
-require_once("inc/funcs/template-layouts.php");
-require_once("inc/funcs/template-helpers.php");
-require_once('inc/funcs/template-main.php');
+require_once("inc/feature.php");
+require_once("inc/funcs/layouts.php");
+require_once("inc/funcs/helpers.php");
+require_once('inc/funcs/main.php');
 require_once("inc/register-acf-blocks.php");
 require_once("inc/cssjs.php");
