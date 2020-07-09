dae12d04383dfce19bc6019260512b34cc39558b
Ernani Danting <danny@lancemontana.com.au>
2020-07-10 08:15:19 +1000

    Edited

--- urbosa/entry.js
+++ urbosa/entry.js
@@ -1,7 +1,3 @@
 require('./assets/lib/semantic-ui/semantic.min.js')
 require('./assets/lib/slick/slick.min.js')
 require('./assets/lib/helper.js')
-
-/* Enable feature */
-// require('./assets/lib/prepareTransition.min.js')
-// require('./assets/lib/curtain/curtain.js')
