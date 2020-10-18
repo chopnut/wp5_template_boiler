edb7a1811d2c8e04b566e53d6280a59b4a6fbb59
Danny Danting <dannydanting@Laurences-MacBook-Pro-Old.local>
2020-10-18 10:14:40 +1000

    Edited

--- urbosa/front-page.php
+++ urbosa/front-page.php
@@ -9,49 +9,4 @@
   }
 
 ?>
-  <div class="test-1 ui transition hidden block">
-    <p>
-  Laborum architecto et quibusdam. Quae provident non. Asperiores magnam consequatur id pariatur et. Numquam consequatur deleniti et reprehenderit ut qui. Ut consequatur omnis reprehenderit dignissimos. Harum qui facere placeat et distinctio.</p>
- 
- Debitis atque non consectetur dicta veniam sit omnis et qui. Ut et consequatur qui voluptatum. Harum repellendus illum. Et in vero reiciendis magnam occaecati rerum vero consequuntur.
-  
- Ipsam soluta natus sint. Exercitationem sapiente excepturi. Et voluptatem ex sed explicabo aut quam. Voluptate laborum officiis occaecati sed laudantium itaque magnam qui. Nemo totam quia eos natus.
-  </div>
-  <div class="test-2 ui transition hidden block">
-    <p>
-      Laborum architecto et quibusdam. Quae provident non. Asperiores magnam consequatur id pariatur et. Numquam consequatur deleniti et reprehenderit ut qui. Ut consequatur omnis reprehenderit dignissimos. Harum qui facere placeat et distinctio.
-    </p>
- 
- Debitis atque non consectetur dicta veniam sit omnis et qui. Ut et consequatur qui voluptatum. Harum repellendus illum. Et in vero reiciendis magnam occaecati rerum vero consequuntur.
-  
- Ipsam soluta natus sint. Exercitationem sapiente excepturi. Et voluptatem ex sed explicabo aut quam. Voluptate laborum officiis occaecati sed laudantium itaque magnam qui. Nemo totam quia eos natus.
-  </div>
-<div>
-<p>
-      Laborum architecto et quibusdam. Quae provident non. Asperiores magnam consequatur id pariatur et. Numquam consequatur deleniti et reprehenderit ut qui. Ut consequatur omnis reprehenderit dignissimos. Harum qui facere placeat et distinctio.
-    </p>
- 
- Debitis atque non consectetur dicta veniam sit omnis et qui. Ut et consequatur qui voluptatum. Harum repellendus illum. Et in vero reiciendis magnam occaecati rerum vero consequuntur.
-  
- Ipsam soluta natus sint. Exercitationem sapiente excepturi. Et voluptatem ex sed explicabo aut quam. Voluptate laborum officiis occaecati sed laudantium itaque magnam qui. Nemo totam quia eos natus.
-</div>
-<div>
-<p>
-      Laborum architecto et quibusdam. Quae provident non. Asperiores magnam consequatur id pariatur et. Numquam consequatur deleniti et reprehenderit ut qui. Ut consequatur omnis reprehenderit dignissimos. Harum qui facere placeat et distinctio.
-    </p>
- 
- Debitis atque non consectetur dicta veniam sit omnis et qui. Ut et consequatur qui voluptatum. Harum repellendus illum. Et in vero reiciendis magnam occaecati rerum vero consequuntur.
-  
- Ipsam soluta natus sint. Exercitationem sapiente excepturi. Et voluptatem ex sed explicabo aut quam. Voluptate laborum officiis occaecati sed laudantium itaque magnam qui. Nemo totam quia eos natus.
-</div>
-<div class="test-1 ui transition hidden block">
-<p>
-      Laborum architecto et quibusdam. Quae provident non. Asperiores magnam consequatur id pariatur et. Numquam consequatur deleniti et reprehenderit ut qui. Ut consequatur omnis reprehenderit dignissimos. Harum qui facere placeat et distinctio.
-    </p>
- 
- Debitis atque non consectetur dicta veniam sit omnis et qui. Ut et consequatur qui voluptatum. Harum repellendus illum. Et in vero reiciendis magnam occaecati rerum vero consequuntur.
-  
- Ipsam soluta natus sint. Exercitationem sapiente excepturi. Et voluptatem ex sed explicabo aut quam. Voluptate laborum officiis occaecati sed laudantium itaque magnam qui. Nemo totam quia eos natus.
-</div>
-  More content here
 <?php get_footer(); ?>
